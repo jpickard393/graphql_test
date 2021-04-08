901117ee4063c758afd9e3e84b49fd10c8c4f832
jpickard393 <jpickard@and.digital>
2021-04-08 13:17:14 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import schema from "./schema";
+import { graphqlHTTP } from "express-graphql";
+
+const app = express();
+
+// default response
+app.get('/', (req, res) => {
+    res.send("GraphQL is amazing");
+});
+
+const root = { hello: () => "Hi I'm graphql" };
+
+// show the graphiql query environmnt
+app.use("/graphql", graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+
+app.listen(8080, () => console.log("Running on server port localhost:8080/graphql"));
